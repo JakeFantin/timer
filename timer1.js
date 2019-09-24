26ac4a456c65c85302b5af1ccc8db0652a5fa7db
Jake <jakefantin@gmail.com>
2019-09-24 23:23:47 +0000

    timer 1 added

---
+++ timer1.js
@@ -0,0 +1,15 @@
+args = process.argv.slice(2);
+
+const beep = (number) => {
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, number * 1000);
+}
+console.log(typeof args[0]);
+for(const num of args){
+  console.log(typeof num);
+  if(num > 0){
+    console.log(num);
+    beep(num);
+  }
+} 
